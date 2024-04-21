9fee7d750ba0940cf6f04554b939aefe1f5ce7e8
Chirag Sharma <chirag@Chirags-MacBook-Pro.local>
2024-04-21 16:42:10 +0530

    init

--- spamemaildetect.py
+++ spamemaildetect.py
@@ -3,18 +3,6 @@ import streamlit as st
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
-# These modules are causing issues when hosting on Streamlit
-# from win32com.client import Dispatch
-# import pythoncom
-# import win32api
-
-# No need to initialize COM objects when running on Streamlit
-# pythoncom.CoInitialize()
-
-# Commenting out the speak function as it requires win32com which is not compatible with Streamlit
-# def speak(text):
-#     speak = Dispatch(("SAPI.SpVoice"))
-#     speak.Speak(text)
 
 # Load the model and CountVectorizer
 model = pickle.load(open('model.pkl', 'rb'))
@@ -44,12 +32,8 @@ def main():
             result = model.predict(vec)
             if result[0] == 0:
                 st.success("This is Not A Spam Email")
-                # Commenting out the speak function as it requires win32com which is not compatible with Streamlit
-                # speak("THANK GOD This is Not A Spam Email")
             else:
                 st.error("This is A Spam Email")
-                # Commenting out the speak function as it requires win32com which is not compatible with Streamlit
-                # speak("ALERT This is A Spam Email")
                 
     else:
         st.write(':green[Spam Email Detection]')
@@ -57,6 +41,3 @@ def main():
         st.write('Filter out you Suspicious By using this Spam Mail Detector Tool that will tell You Weather a Email is a Malicious Email or not. This Spam Email detector saves users valuable time and enhances productivity by ensuring that only relevant and legitimate messages reach their inboxes. Moreover, they serve as a frontline defense against various cyber threats, such as phishing attempts, malware distribution, and scams, thereby bolstering security and protecting sensitive information. Ultimately, This Email Spam detector fosters a positive email experience by delivering a clean and trustworthy inbox, thereby enhancing user satisfaction and trust in email communication.')
 
 main()
-
-
-
